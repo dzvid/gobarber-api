4ab2460a04ab494aba8e169fa8ff4d11c3e1caf9
tukno <oliveiradavid007@gmail.com>
2020-02-16 16:25:11 -0400

    add: CORS

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -29,6 +30,8 @@ class App {
   middlewares() {
     // The request handler must be the first middleware on the app
     this.server.use(Sentry.Handlers.requestHandler());
+
+    this.server.use(cors());
 
     // Configuro envio e recebimento de requisições no formato JSON
     this.server.use(express.json());
