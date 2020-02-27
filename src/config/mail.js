e262d81adb72799960b8d5f6e4e8a5c320c84422
tukno <oliveiradavid007@gmail.com>
2020-02-27 01:56:29 -0400

    update: Mail service module

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,7 +1,7 @@
 export default {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_SECURE,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
